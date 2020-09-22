897fffa9fa037f08b26e28d944550121cf0ce792
jumana1508 <68145608+jumana1508@users.noreply.github.com>
2020-09-22 10:20:42 +0600

    Final

---
+++ Script/1.asm
@@ -0,0 +1,42 @@
+
+
+.MODEL SMALL
+.STACK 100H
+.DATA
+
+CR EQU 0DH
+LF EQU 0AH
+
+MSG1 DB ' LOWER CASE :  $'
+MSG2 DB 0DH,0AH, ' UPPER CASE : '
+CHAR DB ?,'$'
+
+.CODE
+
+MAIN PROC
+    
+    MOV AX, @DATA       
+    MOV DS,AX           
+    
+    
+    LEA DX,MSG1         
+    MOV AH,9            
+    INT 21H           
+   
+    
+    MOV AH,1           
+    INT 21H             
+    SUB AL, 20H         
+    MOV CHAR, AL        
+    
+    
+    LEA DX,MSG2        
+    MOV AH,9            
+    INT 21H             
+    
+   
+    MOV AH,4CH
+    INT 21H             
+    
+MAIN ENDP
+    END MAIN
